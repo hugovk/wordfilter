ffa93af082b6cdeba7386fa5869c9274d1387868
fitnr <fitnr@fakeisthenewreal>
2015-10-13 18:35:42 -0400

    add Makefile task for creating rst

--- setup.py
+++ setup.py
@@ -2,23 +2,9 @@
 from setuptools import setup
 
 try:
-    from pypandoc import convert
-
-    def read_md(f):
-        try:
-            return convert(f, 'rst')
-        except IOError:
-            return ''
-
-except ImportError:
-    print("pypandoc module not found, could not convert Markdown to RST")
-
-    def read_md(f):
-        try:
-            return open(f, 'r').read()
-        except IOError:
-            return ''
-
+    readme = open('README.md', 'r').read()
+except IOError:
+    readme = ''
 
 setup(
     name=u'wordfilter',
@@ -27,7 +13,7 @@ setup(
 
     description="A small module meant for use in text generators that lets you filter strings for bad words.",
 
-    long_description=read_md('readme.md'),
+    long_description=readme,
 
     author=u'Darius Kazemi',
 

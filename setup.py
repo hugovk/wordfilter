5c555d7a0f78e10f71f3a9904e4c949b6a76a20d
fitnr <fitnr@fakeisthenewreal>
2015-10-13 19:18:40 -0400

    bump version

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ except IOError:
     readme = ''
 
 setup(
-    name=u'wordfilter',
+    name='wordfilter',
 
-    version='0.1.8',
+    version='0.1.9',
 
     description="A small module meant for use in text generators that lets you filter strings for bad words.",
 
     long_description=readme,
 
-    author=u'Darius Kazemi',
+    author='Neil Freeman & Jim Witschey, based on work by Darius Kazemi',
 
-    author_email=u'darius.kazemi@gmail.com',
+    author_email='darius.kazemi@gmail.com',
 
     url='http://tinysubversions.com',
 
